c9a327bd9ff9488121333f69e9e66fac8e6c1a78
Sneaky Alec <37029886+SneakyAlec@users.noreply.github.com>
2018-10-30 20:37:24 +0200

    Electron files without build

---
+++ main.js
@@ -0,0 +1,38 @@
+// Electron app and BrowserWindow class
+const { app, BrowserWindow } = require("electron")
+
+// Pastreaza o referinta globala variabilei win
+let win
+
+function createWindow () {
+    // Creaza o fereastra de browser
+    win = new BrowserWindow({ width:800, height:600 })
+
+    // Incarca fisierul in fereastra de browser
+    win.loadFile("index.html")
+
+    // Deschide kitul pentru dezvoltatori
+    win.webContents.openDevTools()
+
+    // Se executa cand fereastra este inchisa
+    win.on("closed", () => {
+        // In momentul in care fereastra este inchisa trebuie sa golim variabila ferestrei de browser
+        win = null
+    })
+}
+
+app.on("ready", createWindow)
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+})
+
+app.on("activate", () => {
+    // Pe macOS, in momentul in care inchidem aplicatia, trebuie sa o redeschidem atunci
+    // cand apasam iconita de pe dock
+    if ( win == null ) {
+        createWindow()
+    }
+})
